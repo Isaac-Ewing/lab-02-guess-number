f8419c93d549105d1437eb2fa72829b298893a00
Isaac <isaac.ewing3@gmail.com>
2021-05-04 20:00:41 -0600

    Got the reset button and wins/losses. Needs styling, TDD, putting UI work into functions

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const guess = document.getElementById('guess-input');
 const submitButton = document.getElementById('submit');
 const guessesLeft = document.getElementById('guesses-left');
 const dispResult = document.getElementById('result');
+const resetButton = document.getElementById('reset');
 
 let guesses = 4;
-let key = 0;   //To enable/disable the 'guess' button
+let reset = 0;
 let losses = 0;
 let wins = 0;
-const answer = Math.ceil(Math.random() * 20);
+let answer = Math.ceil(Math.random() * 20);
 
-if ((guesses > 0) && (key === 0)) {
-    submitButton.addEventListener('click', () => {
+submitButton.addEventListener('click', () => {
+    if ((guesses > 0) && (reset === 0)) {
         console.log(answer);
         guesses--;
         guessesLeft.textContent = guesses;
@@ -22,13 +23,21 @@ if ((guesses > 0) && (key === 0)) {
         }
         if (Number(guess.value) === answer) {
             wins++;
-            dispResult.textContent = `Correct!`;
-            key = 1;
+            dispResult.textContent = `Correct! You have ${wins} wins and ${losses} losses. Try again?`;
+            reset = 1;
         }
         if (Number(guess.value) !== answer && guesses === 0) {
             losses++;
             dispResult.textContent = `Wrong and out of guesses. You have ${wins} wins and ${losses} losses. Try again?`;
-            key = 1;
+            reset = 1;
         };
-    });
-};
+    };
+});
+
+resetButton.addEventListener('click', () => {
+    guesses = 4;
+    guessesLeft.textContent = guesses;
+    answer = Math.ceil(Math.random() * 20);
+    dispResult.textContent = ``;
+    reset = 0;
+});
